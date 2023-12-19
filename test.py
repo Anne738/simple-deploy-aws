711cbad40a28438ecc167f06aaae50ddfbd24426
Anne738 <94481296+Anne738@users.noreply.github.com>
2023-12-19 22:41:35 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,21 @@
 import unittest
+import app as tested_app
+import json
+
+class FlaskAppTests(unittest.TestCase):
+
+    def setUp(self):
+        self.app = tested_app.app.test_client()
+
+    def test_get_hello_endpoint(self):
+        r = self.app.get('/')        
+        self.assertEqual(r._status_code, 200) 
+        self.assertEqual(r.get_data(), b'Hello world from app Pipeline testing V2')    
+
+if __name__ == '__main__':
+    unittest.main()
+
+'''import unittest
 import prog
 
 class TestOrderStack(unittest.TestCase):
@@ -37,4 +54,4 @@ class TestOrderStack(unittest.TestCase):
 
 
 if __name__ == "__main__":
-    unittest.main()
+    unittest.main()'''
